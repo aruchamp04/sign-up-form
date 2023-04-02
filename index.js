9905ec524b50bb214e3fff5e9df057af40e69a1f
aruchamp04 <117742692+aruchamp04@users.noreply.github.com>
2023-04-02 18:03:02 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+
+
+ function myFunction() {
+    var name = document.getElementById("firstName").value;
+    document.getElementById("greeting").innerHTML = "<h2>Hello  " + name + " Welcome!!</h2>";
+  }
+
